7074c9cf2f2600c7d2316f270f464d09687d4c84
Surbhi83 <surabhi.gupta.sai@gmail.com>
2018-10-13 23:43:43 -0400

    Fixed http to https

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends React.Component {
     const country = e.target.elements.country.value
     e.preventDefault()
     const api_call = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key1}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key1}`
     )
     const response = await api_call.json()
     console.log(response)
