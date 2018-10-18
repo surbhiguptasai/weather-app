f948ac03efc58b75f0901e87f164b7b3a5bf4df8
Surbhi Gupta <surabhi.gupta.sai@gmail.com>
2018-10-17 22:51:16 -0400

    Navbar

--- src/components/titles.js
+++ src/components/titles.js
@@ -2,12 +2,12 @@ import React from 'react'
 class Titles extends React.Component {
   render() {
     return (
-      <div>
+      <div className="title-center">
         <h1 className="title-container__title">Weather Scanner </h1>
-        <p className="title-container__subtitle">
+        <h2 className="title-container__subtitle">
           {' '}
           Helps you find weather conditions in cities...{' '}
-        </p>
+        </h2>
       </div>
     )
   }
