1250132ec6c84d4ea8da48a52c92b73b7e269f48
Surbhi83 <surabhi.gupta.sai@gmail.com>
2018-10-17 23:35:45 -0400

    Add Button Functionality

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,30 +1,63 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { fetchWeather, setDay, setUnit } from '../store'
+import {addCity,getCities} from '../store/cities'
+import { getHistoricalWeatherData } from '../store/historicalData'
+import { withRouter } from 'react-router-dom'
 // import Search from './search'
 
-function Weather(props) {
-  const {
-    location,
-    latitude,
-    longitude,
-    date,
-    summary,
-    lo,
-    hi,
-    handleYesterDay,
-    handleTomorrow,
-    day,
-    unit,
-    handleUnitChange,
-  } = props
+class Weather extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      code: '',
+    location: '',
+    latitude: '',
+    longitude:'',
+    date:'',
+    summary:'',
+    lo:'',
+    hi:'',
+    handleYesterDay:'',
+    handleTomorrow:'',
+    day:'',
+    unit:'',
+    handleUnitChange:'',
+    }
 
+    this.submitCity = this.submitCity.bind(this)
+  }
+  
+
+  async submitCity(e)  {
+    e.preventDefault()
+    if (this.props.location !== '') {
+      this.setState({code: ''})
+      await this.props.addCity(this.props.location)
+      // this.props.getCities();
+      
+      //await this.props.addCity("ISELIN,US")
+      console.log("Cities is "+this.props.cities)
+       this.props.getHistoricalWeatherData(this.props.cities)
+    }
+  }
+  
+  render() {
   return (
     <div className="weather">
       {/* <Search /> */}
       <div className="weather-info">
         <div className="weather-toggle">
-          <p className="weather-location">{location}</p>
+          <p className="weather-location">{this.props.location}</p>
+          <span className="input-group-btn">
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={this.submitCity}
+            >
+              Add
+            </button>
+          </span>
         </div>
 
         {/* <button
@@ -34,7 +67,7 @@ function Weather(props) {
               handleYesterDay(latitude, longitude, day)
             }}
           /> */}
-        <h3 className="weather-date">{date}</h3>
+        <h3 className="weather-date">{this.props.date}</h3>
         {/* <button
             className="glyphicon glyphicon-menu-right"
             disabled={day >= 7}
@@ -44,7 +77,7 @@ function Weather(props) {
           />*/}
 
         <p className="weather-summary">
-          <h3>{summary}</h3>
+          <h3>{this.props.summary}</h3>
         </p>
         {/* <div className="weather-icon">
           <Skycon icon={formattedIcon || 'CLOUDY'} />
@@ -53,7 +86,7 @@ function Weather(props) {
         <div className="weather-temp">
           <div className="weather-unit">
             <form>
-              <select name="unit" onChange={handleUnitChange}>
+              <select name="unit" onChange={this.props.handleUnitChange}>
                 <option value="F">F</option>
                 <option value="C">C</option>
               </select>
@@ -62,13 +95,13 @@ function Weather(props) {
           <div className="weather-temp-nums">
             <p>
               Low:{' '}
-              {unit === 'F' ? Math.round(lo) : Math.round((lo - 32) * (5 / 9))}°
-              {unit}{' '}
+              {this.props.unit === 'F' ? Math.round(this.props.lo) : Math.round((this.props.lo - 32) * (5 / 9))}°
+              {this.props.unit}{' '}
             </p>
             <p>
               High:{' '}
-              {unit === 'F' ? Math.round(hi) : Math.round((hi - 32) * (5 / 9))}°
-              {unit}
+              {this.props.unit === 'F' ? Math.round(this.props.hi) : Math.round((this.props.hi - 32) * (5 / 9))}°
+              {this.props.unit}
             </p>
           </div>
         </div>
@@ -79,26 +112,22 @@ function Weather(props) {
     </div>
   )
 }
-
-const mapStateToProps = function(state) {
-  return {
-    // location: state.user.location,
-    // longitude: state.user.longitude,
-    // latitude: state.user.latitude,
-    date: state.weather.date,
-    summary: state.weather.summary,
-    icon: state.weather.icon,
-    precip: state.weather.precip,
-    lo: state.weather.lo,
-    hi: state.weather.hi,
-    day: state.weather.day,
-    unit: state.weather.unit,
-    preciptype: state.weather.preciptype,
-    location: state.weather.location,
-  }
 }
-
-const mapDispatchToProps = function(dispatch) {
+const mapStateToProps = state => ({
+  date: state.weather.date,
+  summary: state.weather.summary,
+  icon: state.weather.icon,
+  precip: state.weather.precip,
+  lo: state.weather.lo,
+  hi: state.weather.hi,
+  day: state.weather.day,
+  unit: state.weather.unit,
+  preciptype: state.weather.preciptype,
+  location: state.weather.location,
+  cities:state.cities
+ })
+ 
+ const mapDispatch = dispatch => {
   return {
     handleYesterDay(latitude, longitude, day) {
       if (day > 0) {
@@ -115,10 +144,15 @@ const mapDispatchToProps = function(dispatch) {
     handleUnitChange(event) {
       dispatch(setUnit(event.target.value))
     },
+    addCity: city => dispatch(addCity(city)),
+    getHistoricalWeatherData: cities => dispatch(getHistoricalWeatherData(cities))
   }
-}
+   }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Weather)
+   
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatch
+  )(Weather)
+)
